c32ca3f85e81ce8b144dbc66578ff4ba2a316f5f
lukgla <52761889+lukgla@users.noreply.github.com>
2024-04-05 01:28:25 +0200

    final cleanup

--- 2018/5b/5b.srcs/sources_1/ip/video_mem/video_mem_stub.vhdl
+++ 2018/5b/5b.srcs/sources_1/ip/video_mem/video_mem_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
--- Date        : Fri Apr  5 01:05:39 2024
+-- Date        : Fri Apr  5 01:16:33 2024
 -- Host        : DESKTOP-L54QMU3 running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub
 --               C:/Users/lukgla/Desktop/Vivado/2018/5b/5b.srcs/sources_1/ip/video_mem/video_mem_stub.vhdl
