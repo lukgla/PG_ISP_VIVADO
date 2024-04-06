df87be02c6073edc3c2a8cb749e1ee2d4783d32f
lukgla <52761889+lukgla@users.noreply.github.com>
2024-04-07 01:49:15 +0200

    6 extended untested

--- vhd/6/tb_6.vhd
+++ vhd/6/tb_6.vhd
@@ -13,6 +13,9 @@ architecture Behavioral of tb_6 is
   signal led7_an_o  : std_logic_vector(3 downto 0);
   signal led7_seg_o : std_logic_vector(7 downto 0);
 
+
+  signal clk_o : STD_LOGIC;
+
 begin
     clk_i <= not clk_i after 5 ns;
     top_6_inst: entity work.top_6
@@ -24,9 +27,21 @@ begin
       led7_seg_o => led7_seg_o
     );
 
-    buttons: process (clk_i) is
+    clockdivider_inst: entity work.clockDivider
+    generic map (
+      one_cycles    => 10_000,
+      zero_cycles   => 10_000,
+      initial_state => '0'
+    )
+    port map (
+      clk_i => clk_i,
+      rst_i => rst_i,
+      clk_o => clk_o
+    );
+
+    buttons: process (clk_o) is
     begin
-        if rising_edge(clk_i) then
+        if rising_edge(clk_o) then
             button_i <= std_logic_vector(unsigned(button_i) + 1);
         end if ;
     end process;
